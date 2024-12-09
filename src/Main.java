4b9ae507b7ae5bebef2be18cc1a60f439840e6a1
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-09 18:04:44 +0000

    added print number logic

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,29 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        printNumberInWord(-1);
+        printNumberInWord(0);
+        printNumberInWord(5);
+        printNumberInWord(9);
+        printNumberInWord(10);
+    }
 
+    public static void printNumberInWord(int number) {
+
+        String strNumber = switch (number) {
+            case 0 -> "ZERO";
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        };
+
+        System.out.println(strNumber);
     }
 }
